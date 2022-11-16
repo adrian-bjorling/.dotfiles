739b762f9a97658aecc26815367efd05e935e145
adrian-bjorling <98982479+adrian-bjorling@users.noreply.github.com>
2022-11-16 19:30:31 +0100

    M emacs/.emacs.d/config.org  M emacs/.emacs.d/init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit projectile which-key use-package ivy-rich hydra helpful general evil-collection doom-themes doom-modeline counsel all-the-icons)))
+   '(blacken pyvenv web-beautify web-mode svelte-mode add-node-modules-path js-doc prettier-js rjsx-mode js2-refactor js2-mode yasnippet-snippets yasnippet eglot flycheck dumb-jump exec-path-from-shell deadgrep fzf rainbow-mode smartparens benchmark-init atom-one-dark-theme zenburn-theme forge evil-magit magit counsel-projectile projectile which-key use-package rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-themes doom-modeline counsel command-log-mode all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
